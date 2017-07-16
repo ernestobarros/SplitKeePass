5729a7aff64d221b2aa43bc0b85b8208b42e0063
Ernesto Barros <e.leon.barros@gmail.com>
2017-07-16 17:17:59 +0200

    Initial: Split KeePass entries

---
+++ Install-KeePassPackages.fsx
@@ -0,0 +1,59 @@
+﻿// Run the steps separately with F# interactive
+// Select the code for the step and choose ALT+ENTER
+
+//------------------------------------------
+// Step 0. Get the package bootstrap. This is standard F# boiler plate for scripts that also get packages.
+open System
+open System.IO
+let packagesDir = __SOURCE_DIRECTORY__ + "/script-packages"
+Directory.CreateDirectory(packagesDir)
+Environment.CurrentDirectory <- packagesDir
+if not (File.Exists "paket.exe") then
+    let url = "https://github.com/fsprojects/Paket/releases/download/1.2.0/paket.bootstrapper.exe" in use wc = new System.Net.WebClient() in let tmp = Path.GetTempFileName() in wc.DownloadFile(url, tmp); File.Move(tmp,"paket.bootstrapper.exe"); System.Diagnostics.Process.Start("paket.bootstrapper.exe") |> ignore;;
+
+//------------------------------------------
+// Step 1. Resolve and install the Canopy package and the Chrome web driver
+// You can add any additional packages you like to this step.
+#r "script-packages/paket.exe"
+Paket.Dependencies.Install """
+group Security
+    source https://nuget.org/api/v2
+    nuget KeePassLib
+    github saintedlama/keepasschrome:63d150f sample/sample.kdbx
+    github twsouthwick/KeePassLib:f868002 KeePassLibTest/Assets/password-aes_rijndael_256.kdbx
+""";;
+
+//------------------------------------------
+// Step 2. Reference the .. framework libraries
+#r @"script-packages/packages/security/KeePassLib/lib/KeePassLib.dll"
+open KeePassLib
+open KeePassLib.Keys
+open KeePassLib.Serialization
+
+//------------------------------------------
+// Step 3. Sample files
+let dbPath = "paket-files\security\saintedlama\keepasschrome\sample\sample.kdbx"
+// let dbPath = "paket-files/security/twsouthwick/KeePassLib/KeePassLibTest/Assets/password-aes_rijndael_256.kdbx"
+let key = CompositeKey()
+key.AddUserKey(KcpPassword("password"))
+// key.AddUserKey(new KcpPassword("12345"))
+let db = PwDatabase()
+db.Open(IOConnectionInfo(Path = dbPath), key, null)
+
+let printPwEntry (entry: PwEntry) =
+    printfn @"
+    Group    %s
+    Title    %s
+    Username %s
+    Password %s
+    URL      %s
+    Notes    %s
+    "
+    <| entry.ParentGroup.Name
+    <| entry.Strings.ReadSafe("Title")
+    <| entry.Strings.ReadSafe("UserName")
+    <| entry.Strings.ReadSafe("Password")
+    <| entry.Strings.ReadSafe("URL")
+    <| entry.Strings.ReadSafe("Notes")
+db.RootGroup.GetEntries true
+|> Seq.iter printPwEntry
---
+++ SplitKeePass.fsx
@@ -0,0 +1,199 @@
+﻿#r @"script-packages/packages/security/KeePassLib/lib/KeePassLib.dll"
+open KeePassLib
+open KeePassLib.Keys
+open KeePassLib.Serialization
+open KeePassLib.Security
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+type BridgeEntry =
+    {
+        title: string
+        url: string
+        pass: string
+    }
+
+type ConfigSettings =
+    {
+        bridgePath: string;
+        bridgePass: string;
+        bridgeEntries: BridgeEntry list
+        sourcePath: string;
+        sourcePass: string
+        targetPath: string;
+        targetPass: string
+    }
+
+let config =
+    {
+        bridgePath = "DummyBridgeDB.kdbx"
+        bridgePass = null
+        bridgeEntries = [
+                        {title = "ProdDB"; pass = "1"; url = "DummySourceDB.kdbx"}
+                        {title = "TestDB"; pass = "2"; url = "DummyTargetDB.kdbx"}
+                        ]
+        sourcePath = "DummySourceDB.kdbx"
+        sourcePass = "1"
+        targetPath = "DummyTargetDB.kdbx"
+        targetPass = "2"
+    }
+
+let makeCompositeKey (dbPass: string) =
+    let key = CompositeKey()
+    if isNull dbPass then
+        // Use the current Windows user account
+        key.AddUserKey(KcpUserAccount())
+    else
+        key.AddUserKey(KcpPassword(dbPass))
+    key
+
+let getDB (dbPath: string) (dbPass: string) =
+    Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+    let key = makeCompositeKey dbPass
+    let db = PwDatabase()
+    db.Open(IOConnectionInfo(Path = dbPath), key, null)
+    db
+
+let createKdbxDatabase dbPath dbPass =
+    Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+    let db = PwDatabase()
+    db.Name <- Path.GetFileNameWithoutExtension(dbPass)
+    db.MasterKey <- makeCompositeKey dbPass
+    let kdbx = KdbxFile(db)
+    let groupDataSoucrce = PwGroup(true, true)
+    use streamToFile = File.OpenWrite(dbPath)
+    kdbx.Save(streamToFile, groupDataSoucrce, KdbxFormat.Default, null)
+
+let createBridgeDB () =
+    let dbPath = config.bridgePath
+    let dbPass = config.bridgePass
+    if File.Exists dbPath then File.Delete dbPath
+    createKdbxDatabase dbPath dbPass
+    let db = getDB dbPath dbPass
+    let addEntry (bridgeEntry: BridgeEntry) =
+        PwEntry(true, true)
+        |> fun entry ->
+            entry.IconId <- PwIcon.Settings
+            entry.Strings.Set(PwDefs.TitleField, ProtectedString(false, bridgeEntry.title))
+            entry.Strings.Set(PwDefs.UrlField, ProtectedString(false, bridgeEntry.url))
+            entry.Strings.Set(PwDefs.PasswordField, ProtectedString(true, bridgeEntry.pass))
+            db.RootGroup.AddEntry(entry, true)
+    // Populate the bridge DB
+    config.bridgeEntries |> List.iter addEntry
+    db.Save(null)
+    db.Close()
+
+let createSourceDB () =
+    let dbPath = config.sourcePath
+    let dbPass = config.sourcePass
+    if File.Exists dbPath then File.Delete dbPath
+    createKdbxDatabase dbPath dbPass
+    let db = getDB dbPath dbPass
+    let addGroup (groupName: string) =
+        PwGroup(true, true)
+        |> fun group ->
+            group.IconId <- PwIcon.Configuration
+            group.Name <- groupName
+            db.RootGroup.AddGroup(group, true, true)
+    // Populate the source DB with groups
+    ["Database"; "FTP"] |> List.iter addGroup
+    let addEntry (title: string, user: string, pass: string) =
+        PwEntry(true, true)
+        |> fun entry ->
+            entry.IconId <- PwIcon.Settings
+            entry.Strings.Set(PwDefs.TitleField, ProtectedString(false, title))
+            entry.Strings.Set(PwDefs.UserNameField, ProtectedString(false, user))
+            entry.Strings.Set(PwDefs.PasswordField, ProtectedString(true, pass))
+            db.RootGroup.GetGroups(false)
+            |> Seq.filter (fun g -> g.Name = "Database")
+            |> Seq.exactlyOne
+            |> fun g -> g.AddEntry(entry, true)
+    // Populate the source DB with entries
+    [
+        ("XXX-SQLTEST.app1", "app1user", "1")
+        ("XXX-SQLTEST.app2", "app2user", "2")
+    ] |> List.iter addEntry
+    db.Save(null)
+    db.Close()
+
+let createTargetDB () =
+    let dbPath = config.targetPath
+    let dbPass = config.targetPass
+    if File.Exists dbPath then File.Delete dbPath
+    createKdbxDatabase dbPath dbPass
+
+(*
+    create the dummy databases
+*)
+createBridgeDB ()
+createSourceDB ()
+createTargetDB ()
+
+// DO IT!!
+let getEntries (db: PwDatabase) filterEntries =
+    db.RootGroup.GetEntries true
+    |> Seq.filter filterEntries
+
+let getProdDB () =
+    let dbBridge = getDB config.bridgePath config.bridgePass
+    let getPassDB (entries: seq<PwEntry>) =
+        entries
+        |> Seq.map (fun entry -> (entry.Strings.ReadSafe("URL"), entry.Strings.ReadSafe("Password")))
+        |> Seq.exactlyOne
+    let (dbPath, dbPass) =
+        let filter = fun (entry:PwEntry) -> entry.Strings.ReadSafe("Title") = "ProdDB"
+        getPassDB <| getEntries dbBridge filter
+    dbBridge.Close()
+    let dbProd = getDB dbPath dbPass
+    dbProd
+
+let getTestDB () =
+    let dbBridge = getDB config.bridgePath config.bridgePass
+    let getPassDB (entries: seq<PwEntry>) =
+        entries
+        |> Seq.map (fun entry -> (entry.Strings.ReadSafe("URL"), entry.Strings.ReadSafe("Password")))
+        |> Seq.exactlyOne
+    let (dbPath, dbPass) =
+        let filter = fun (entry:PwEntry) -> entry.Strings.ReadSafe("Title") = "TestDB"
+        getPassDB <| getEntries dbBridge filter
+    dbBridge.Close()
+    let dbTest = getDB dbPath dbPass
+    dbTest
+
+let doSplitDB () =
+    let getSourceEntries () =
+        let sourceDB = getProdDB ()
+        let filterDB = fun (pe:PwEntry) ->
+            [
+                fun _ ->
+                    pe.ParentGroup.Name = "Database"
+                    && Regex.IsMatch(pe.Strings.ReadSafe(PwDefs.TitleField), "XXX-SQLTEST", RegexOptions.IgnoreCase)
+                fun _ ->
+                    pe.ParentGroup.Name = "FTP"
+                    && Regex.IsMatch(pe.Strings.ReadSafe(PwDefs.TitleField), "TEST", RegexOptions.IgnoreCase)
+            ]
+            |> List.exists (fun f -> f())
+        let entries = getEntries sourceDB filterDB
+        sourceDB.Close()
+        entries
+    let addEntries (db: PwDatabase) (entries: PwEntry seq) =
+        let addParentGroup (sourceGroup: PwGroup) =
+            db.RootGroup.AddGroup(PwGroup(true, true, sourceGroup.Name, sourceGroup.IconId), true)
+        let addEntry = fun (pe: PwEntry) ->
+            if isNull <| db.RootGroup.FindCreateGroup(pe.ParentGroup.Name, false) then
+                addParentGroup pe.ParentGroup
+            let targetGroup = db.RootGroup.FindCreateGroup(pe.ParentGroup.Name, false)
+            if isNull <| targetGroup.FindEntry(pe.Uuid, false) then
+                targetGroup.AddEntry(pe, true)
+        entries
+        |> Seq.iter addEntry
+        db.Save(null)
+        db.Close()
+    let targetDB = getTestDB ()
+    let targetEntries = getSourceEntries ()
+    // DO IT!!
+    addEntries targetDB targetEntries
+
+doSplitDB ()
+
